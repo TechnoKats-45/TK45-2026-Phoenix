dc3a32dd12c4476ce559ce443799159c23a7bbe2
Jonathan Lane <Jonathan.lane@andymark.com>
2026-04-09 15:24:58 -0400

    comment

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -33,7 +33,7 @@ public class Intake extends SubsystemBase
     private static final double SUPPLY_CURRENT_LIMIT_AMPS_PIVOT = 60.0;
     private static final double MM_CRUISE_RPS_PIVOT = 1; // TODO TUNE
     private static final double MM_ACCEL_RPS2_PIVOT = 10; // TODO TUNE
-    private static final double SENSOR_TO_MECHANISM_RATIO_PIVOT = 128.0; // TODO TUNE
+    private static final double SENSOR_TO_MECHANISM_RATIO_PIVOT = 80 * 1.6; // 80:1 geabox and 25:50 gear ratio
     private static final double PEAK_FORWARD_VOLTS_PIVOT= 16.0;
     private static final double PEAK_REVERSE_VOLTS_PIVOT= - 16.0; 
     private double currentAngleSetPoint = 0.0;   
